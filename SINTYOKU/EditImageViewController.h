5fd73f2c630e0ed11a102d972b314e0a9252c407
sadakoa <sadako0215@gmail.com>
2014-07-03 12:20:16 +0900

    EditImageViewController.h / m追加

---
+++ SINTYOKU/EditImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditImageViewController.h
+//  SINTYOKU
+//
+//  Created by sadakoa on 2014/07/03.
+//  Copyright (c) 2014年 sadakoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditImageViewController : UIViewController
+
+@end
