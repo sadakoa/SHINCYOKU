500f89887b00e15e9157f0d9c798b1140ed235da
sadakoa <sadako0215@gmail.com>
2014-07-05 03:04:36 +0900

    social.framework追加

---
+++ SINTYOKU/SNSViewController.h
@@ -0,0 +1,13 @@
+//
+//  SNSViewController.h
+//  SINTYOKU
+//
+//  Created by sadakoa on 2014/07/05.
+//  Copyright (c) 2014年 sadakoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SNSViewController : UIViewController
+
+@end
