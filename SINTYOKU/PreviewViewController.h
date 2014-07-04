43346d3b4e0615dbcf618596ccff4fd6f6a43ec8
sadakoa <sadako0215@gmail.com>
2014-07-04 13:37:50 +0900

    プレビュー画面配置

---
+++ SINTYOKU/PreviewViewController.h
@@ -0,0 +1,13 @@
+//
+//  PreviewViewController.h
+//  SINTYOKU
+//
+//  Created by sadakoa on 2014/07/04.
+//  Copyright (c) 2014年 sadakoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PreviewViewController : UIViewController
+
+@end
