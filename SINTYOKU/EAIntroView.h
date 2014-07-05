86cce69d103e57579a6d8fb975f7b3bdee0cb383
sadakoa <sadako0215@gmail.com>
2014-07-05 22:44:35 +0900

    error再発

---
+++ SINTYOKU/EAIntroView.h
@@ -0,0 +1,15 @@
+//
+//  EAIntroView.h
+//  SINTYOKU
+//
+//  Created by sadakoa on 2014/07/05.
+//  Copyright (c) 2014年 sadakoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EAIntroView : UIView
+
+@property(nonatomic,weak) IBOutlet EAIntroView *introView;
+
+@end
---
+++ SINTYOKU/EAIntroView.m
@@ -0,0 +1,31 @@
+//
+//  EAIntroView.m
+//  SINTYOKU
+//
+//  Created by sadakoa on 2014/07/05.
+//  Copyright (c) 2014年 sadakoa. All rights reserved.
+//
+
+#import "EAIntroView.h"
+
+@implementation EAIntroView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
