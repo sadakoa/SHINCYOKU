cafca772fa193d07f75f8ae09cd79db787c31ace
sadakoa <sadako0215@gmail.com>
2014-07-17 11:57:41 +0900

    coachmarks導入

--- SINTYOKU/EditImageViewController.h
+++ SINTYOKU/EditImageViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "WSCoachMarksView.h"
 
 @interface EditImageViewController : UIViewController\
 <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
